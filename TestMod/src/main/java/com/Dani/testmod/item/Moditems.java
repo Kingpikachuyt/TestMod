6904d99ff821b2e32e64eda5a8630aae163a7e8f
Dani <daniel@mrfodder.com>
2025-12-22 23:27:38 +0000

    Added setup for items

---
+++ TestMod/src/main/java/com/Dani/testmod/item/Moditems.java
@@ -0,0 +1,18 @@
+package com.Dani.testmod.item;
+
+import com.Dani.testmod.TestMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
